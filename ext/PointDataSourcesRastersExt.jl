5c0ae33707dd2c3f4767992b1d48cca142103412
mrke <mrke@unimelb.edu.au>
2026-07-07 18:28:02 +1000

    export Zarrsource

--- ext/PointDataSourcesRastersExt.jl
+++ ext/PointDataSourcesRastersExt.jl
@@ -4,6 +4,7 @@ using PointDataSources
 using RasterDataSources: ERA5, ARCO_ERA5_URL, layername
 import RasterDataSources
 using Rasters
+using Rasters.DimensionalData: dims
 using ZarrDatasets
 using Dates
 
@@ -22,7 +23,7 @@ extension).
 """ getpoint(::Type{ERA5}, ::Symbol; kw...)
 
 function getpoint(T::Type{ERA5}, layer::Symbol; lon, lat, date)
-    stack = RasterStack(ARCO_ERA5_URL; source=Zarrsource(), lazy=true)
+    stack = RasterStack(ARCO_ERA5_URL; source=Rasters.Zarrsource(), lazy=true)
     start, finish = PointDataSources._point_daterange(date)
     r = stack[layername(T, layer)][X(Near(lon)), Y(Near(lat)), Ti(start .. finish)]
     times = Date.(collect(dims(r, Ti)))
